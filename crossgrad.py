Fixed gradients and clippings + reusing on implementation

crossgrad.py
@@ -43,7 +43,7 @@ def crossgrad_domain_fn(features, labels, mode, params):
     return domain, tf.reduce_mean(loss)
 
 
-def crossgrad(mode, label_fn, x, domain, labels, epsilon_d, epsilon_l, alpha_d, alpha_l, latent_fn=None, domain_fn=None, latent_space_dimensions=100, params=None):    
+def crossgrad(mode, label_fn, x, domain, labels, epsilon_d, epsilon_l, alpha_d, alpha_l, latent_fn=None, domain_fn=None, latent_domain_fn=None, latent_space_dimensions=100, params=None):
     # Some default parameters
     if params is None: params = {}
     
@@ -60,40 +60,71 @@ def crossgrad(mode, label_fn, x, domain, labels, epsilon_d, epsilon_l, alpha_d, 
     x = to_data_format(x, 'channels_last', params['data_format'])
     
     def forward(x_l, x_d, d, labels, mode, params):
-        latent = latent_fn(features=x_l, labels=None, mode=mode, params=params)
-        domain, loss_domain = domain_fn(features=latent, labels=d, mode=mode, params=params)
+        if latent_domain_fn is not None:
+            latent, domain, loss_domain = latent_domain_fn(features=x_l, labels=d, mode=mode, params=params)
+        else:
+            latent = latent_fn(features=x_l, labels=None, mode=mode, params=params)
+            domain, loss_domain = domain_fn(features=latent, labels=d, mode=mode, params=params)
+        latent = tf.stop_gradient(latent)
         outputs, loss_label = label_fn(features=x_d, latent=latent, labels=labels, mode=mode, params=params)
 
         return ((domain, loss_domain), (outputs, loss_label))
     
     # First forward pass
-    ((d1, d1_loss), (l1, l1_loss)) = forward(x, x, domain, labels, mode, params)
-    grad_label_x = tf.gradients(l1_loss, x)[0]
-    grad_domain_x = tf.gradients(d1_loss, x)[0]
-            
-    # Second pass
-    x_d = x + epsilon_d * grad_domain_x
-    x_l = x + epsilon_l * grad_label_x
-    ((d2, d2_loss), (l2, l2_loss)) = forward(x_l, x_d, domain, labels, mode, params)
+    with tf.variable_scope('crossgrad', reuse=False):
+        ((d1, d1_loss), (l1, l1_loss)) = forward(x, x, domain, labels, mode, params)
         
-    l_total_loss = (1 - alpha_l) * l1_loss + alpha_l * l2_loss
-    d_total_loss = (1 - alpha_d) * d1_loss + alpha_d * d2_loss
-    predictions = {
-        'd1': d1,
-        'd2': d2,
-        'l1': l1,
-        'l2': l2
-    }
-    
-    if params.get('summaries'):
-        tf.summary.scalar('loss/domain/1', d1_loss)
-        tf.summary.scalar('loss/labels/1', l1_loss)
-        
-        tf.summary.scalar('loss/domain/2', d2_loss)
-        tf.summary.scalar('loss/labels/2', l2_loss)    
+    if mode != tf.estimator.ModeKeys.PREDICT:    
+        grad_label_x = tf.stop_gradient(tf.gradients(l1_loss, x)[0])
+        grad_domain_x = tf.stop_gradient(tf.gradients(d1_loss, x)[0])
 
-        tf.summary.image('x', to_data_format(x, params['data_format'], 'channels_last'))
-        tf.summary.image('x_d', to_data_format(x_d, params['data_format'], 'channels_last'))
-        tf.summary.image('x_l', to_data_format(x_l, params['data_format'], 'channels_last'))
+        grad_label_x = tf.clip_by_value(grad_label_x, -0.1, 0.1)
+        grad_domain_x = tf.clip_by_value(grad_domain_x, -0.1, 0.1)
+
+        if epsilon_l == 0:
+            grad_label_x = 0
+
+        if epsilon_d == 0:
+            grad_domain_x = 0
+
+        # Second pass
+        with tf.variable_scope('crossgrad', reuse=True):
+            x_d = x + epsilon_d * grad_domain_x
+            x_l = x + epsilon_l * grad_label_x
+            ((d2, d2_loss), (l2, l2_loss)) = forward(x_l, x_d, domain, labels, mode, params)
+
+        l_total_loss = (1 - alpha_l) * l1_loss + alpha_l * l2_loss
+        d_total_loss = (1 - alpha_d) * d1_loss + alpha_d * d2_loss
+        predictions = {
+            'd1': d1,
+            'd2': d2,
+            'l1': l1,
+            'l2': l2
+        }
+
+        if params.get('summaries'):
+            tf.summary.scalar('loss/domain/1', d1_loss)
+            tf.summary.scalar('loss/labels/1', l1_loss)
+
+            tf.summary.scalar('loss/domain/2', d2_loss)
+            tf.summary.scalar('loss/labels/2', l2_loss)    
+
+            tf.summary.image('x', to_data_format(x, params['data_format'], 'channels_last'))
+            tf.summary.image('x_d', to_data_format(x_d, params['data_format'], 'channels_last'))
+            tf.summary.image('x_l', to_data_format(x_l, params['data_format'], 'channels_last'))
+
+            if epsilon_d > 0:
+                tf.summary.image('grad_d', to_data_format(epsilon_d * grad_domain_x, params['data_format'], 'channels_last'))
+                tf.summary.histogram('grad_d', to_data_format(epsilon_d * grad_domain_x, params['data_format'], 'channels_last'))
+
+            if epsilon_l > 0:
+                tf.summary.image('grad_l', to_data_format(epsilon_l * grad_label_x, params['data_format'], 'channels_last'))
+                tf.summary.histogram('grad_l', to_data_format(epsilon_l * grad_label_x, params['data_format'], 'channels_last'))
+    else:
+        predictions = {
+            'd1': d1,
+            'l1': l1
+        }
+        l_total_loss = d_total_loss = 0
     
     return predictions, l_total_loss + d_total_loss